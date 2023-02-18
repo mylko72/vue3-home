c9bd1ab50dbc7a83858790505895c29e14ec8be4
Kwanoh Lee <mypmk@naver.com>
2023-02-18 22:36:51 +0900

    css 수정

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -151,12 +151,5 @@ const goDetail = (id) => {
 </script>
 
 <style scoped>
-.key-visual {
-	height: 500px;
-	background-image: url(/assets/images/img_winter.jpg);
-	background-position: 50% 100%;
-	background-repeat: no-repeat;
-	background-attachment: fixed;
-	background-size: cover;
-}
+
 </style>
